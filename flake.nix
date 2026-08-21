ec39f518d990c616692a6238d86bb0ac048b6f5b
Kristina Olsson <ko@poqpoq.se>
2026-08-21 13:00:24 +0200

    Updated flake with package

--- flake.nix
+++ flake.nix
@@ -2,16 +2,14 @@
   description = "Shared base modules";
   # version 9.7p1
   inputs.nixpkgs-ssh.url = "github:NixOS/nixpkgs/a343533bccc62400e8a9560423486a3b6c11a23b";
-  outputs = { self, nixpkgs-ssh, ... }: {
-    nixosModules.opensshPin =
-      { pkgs, lib, ... }:
-      let
-        pkgs-ssh = import nixpkgs-ssh { system = pkgs.system; };
-      in
-      {
-        environment.systemPackages = [ pkgs-ssh.openssh ];
-        # services.openssh.package = lib.mkDefault pkgs-ssh.openssh; # mkDefault make it overridable
-        services.openssh.package = pkgs-ssh.openssh; # needs to be overriden by mkForc
-      };
-  };
+
+  outputs =
+    { self, nixpkgs-ssh, ... }:
+    let
+      pkgs-ssh = import nixpkgs-ssh { system = "x86_64-linux"; };
+    in
+    {
+      # nixosModules.opensshPin = { ... };
+      packages.x86_64-linux.openssh = pkgs-ssh.openssh;
+    };
 }
