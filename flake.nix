1392f2d81bbc122410773fc1631d7b8a66b03f35
Kristina Olsson <ko@poqpoq.se>
2026-08-21 13:06:13 +0200

    corrected default

--- flake.nix
+++ flake.nix
@@ -11,6 +11,6 @@
     {
       # nixosModules.opensshPin = { ... };
       packages.x86_64-linux.openssh = pkgs-ssh.openssh;
-      packages.default.openssh = pkgs-ssh.openssh;
+      packages.x86_64-linux.default = pkgs-ssh.openssh;
     };
 }
